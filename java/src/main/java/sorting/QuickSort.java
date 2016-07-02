323eeeead2e45d854a4a4b9d2c669f3377fda8d2
Alexandra TUDOR <alexandra.tudor93@gmail.com>
2016-07-03 01:17:39 +0300

    QuickSort

--- java/src/main/java/sorting/QuickSort.java
+++ java/src/main/java/sorting/QuickSort.java
@@ -1,7 +1,42 @@
 package sorting;
 
-public class QuickSort {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class QuickSort {
+
+    public static void sort(int[] a, int low, int high) {
+        if (low >= high)
+            return;
+
+        int pivotIndex = partition(a, low, high);
+
+        sort(a, low, pivotIndex-1);
+        sort(a, pivotIndex+1, high);
+    }
+
+    private static int partition(int[] a, int low, int high) {
+        int pivot = a[high];
+        int i = low;
+
+        for (int j = low; j < high-1; j++) {
+            if (a[j] < pivot) {
+                int aux = a[j];
+                a[j] = a[i];
+                a[i] = aux;
+                i++;
+            }
+        }
+
+        int aux = a[high];
+        a[high] = a[i];
+        a[i] = aux;
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{9, 2, 4, 7, 12, 1, 35, 67, 34, 89, 76};
+        sort(array, 0, 10);
+        System.out.println(Arrays.toString(array));
     }
 }
